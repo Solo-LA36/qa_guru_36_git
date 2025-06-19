db3610bb87bb8eb72bfd00310c24cd110afe64a7
Aleksandr Nedikov <fin82.82@yandex.ru>
2025-06-19 16:09:50 +0300

    Update Main.java

--- src/main/java/ru/Solo_LA36/Main.java
+++ src/main/java/ru/Solo_LA36/Main.java
@@ -1,6 +1,6 @@
 package ru.Solo_LA36;
 
-private static String msg = "!!!!!!!!Hello and welcome!!!!!"
+private static String msg = "Hello and welcome!"
 public class Main {
     public static void main(String[] args) {
         System.out.println(msg);
