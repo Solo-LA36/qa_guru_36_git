7e2649514b313d630241f61a5adaa345286772ac
Solo-LA36 <fin82.82@yandex.ru>
2025-06-19 15:47:09 +0300

    added getMessage() method

--- src/main/java/ru/Solo_LA36/Main.java
+++ src/main/java/ru/Solo_LA36/Main.java
@@ -3,6 +3,11 @@ package ru.Solo_LA36;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!!!!!");
+        System.out.printlngetMessage());
+    }
+
+
+    private static String getMessage() {
+        return ("Hello and welcome!!!!!");
     }
 }
